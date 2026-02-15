b78550f0c751cdc4aafca04d194089d825acfa2c
kiseka914 <attahmercy61@gmail.com>
2026-02-15 15:50:55 -0800

    .

---
+++ js/demo.js
@@ -0,0 +1,15 @@
+function demo() {
+  document.body.append(demoBlock);
+  let opacityValue = 1;
+
+  setInterval(() => {
+    if (opacityValue) {
+      opacityValue = 0;
+      demoBlock.style.opacity = opacityValue;
+    } else {
+      opacityValue = 1;
+      demoBlock.style.opacity = opacityValue;
+    }
+  }, 1400);
+}
+demo();
